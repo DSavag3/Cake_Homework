7d8ab02a78ea17735027554fc1deb90076677145
DSavag3 <dantedsavage@gmail.com>
2023-09-14 23:02:10 -0700

    initial Commit

---
+++ src/main/java/org/example/cake/CakeTester.java
@@ -0,0 +1,11 @@
+package org.example.cake;
+
+import org.example.Mother;
+
+public class CakeTester {
+    public static void main(String[] args) {
+        BirthdayCake birthday = new BirthdayCake();
+
+    }
+
+}
---
+++ src/main/java/org/example/cake/WeddingCake.java
@@ -0,0 +1,5 @@
+package org.example.cake;
+
+public class WeddingCake extends Cake {
+    private int tiers;
+}
